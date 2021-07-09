83fe2e1b5659484c3954221682b74153553626cf
Kenil Cheng <kenilc@gmail.com>
2021-07-09 01:27:24 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ REQUIRED_PACKAGES = [
     'datasets>=1.8.0',
     'google-cloud-storage',
     'numpy>=1.18.5'
-    'pyarrow',
-    'torch',
+    'pyarrow>=3.0.0',
+    'torch>=1.9.0',
     'tqdm',
-    'sentencepiece',
+    'sentencepiece>=0.1.95',
     'transformers>=4.8.2',
 ]
 
